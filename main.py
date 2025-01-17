1f8ff30fde424a5f06102d1251167fbca59e1e60
TheTrueJM <joshuamalinao05@gmail.com>
2025-01-17 12:13:09 +1000

    functionality developed

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
 from flask_sqlalchemy import SQLAlchemy
+from os.path import isfile
 from datetime import datetime
+
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blogposts.db"
 db = SQLAlchemy(app)
-api = Api(app)
+routes = Api(app)
 
 
-
-class PostsModel(db.Model):
+class PostModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     content = db.Column(db.String)
     category = db.Column(db.String)
     tags = db.Column(db.String)
-    createdAt = db.Column(db.DateTime)
-    updatedAt = db.Column(db.DateTime)
+    createdAt = db.Column(db.DateTime, default=datetime.now)
+    updatedAt = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
-class TagField(fields.Raw):
+class TagsField(fields.Raw):
     def output(self, key, posts):
-        return posts.tags.split(",") if type(posts) == PostsModel else []
+        return posts.tags.split(",") if type(posts) == PostModel else []
 
 post_args = reqparse.RequestParser()
 post_args.add_argument("title", type=str, required=True, help="Post requires title")
@@ -35,7 +36,7 @@ post_fields = {
     "title": fields.String,
     "content": fields.String,
     "category": fields.String,
-    "tags": TagField,
+    "tags": TagsField,
     "createdAt": fields.DateTime(dt_format="iso8601"),
     "updatedAt": fields.DateTime(dt_format="iso8601")
 }
@@ -46,56 +47,57 @@ class Posts(Resource):
     @marshal_with(post_fields)
     def get(self):
         term = f"%{request.args.get("term") or ''}%"
-        posts = PostsModel.query.filter(PostsModel.title.like(term) | PostsModel.content.like(term) | PostsModel.category.like(term)).all()
+        posts = PostModel.query.filter(PostModel.title.like(term) | PostModel.content.like(term) | PostModel.category.like(term)).all()
         return posts, 200
     
     @marshal_with(post_fields)
     def post(self):
         args = post_args.parse_args()
-        dt = datetime.now()
-        post = PostsModel(title=args["title"], content=args["content"], category=args["category"], tags=",".join(args["tags"]), createdAt=dt, updatedAt=dt)
+        post = PostModel(title=args["title"], content=args["content"], category=args["category"], tags=",".join(args["tags"]))
         db.session.add(post)
         db.session.commit()
         return post, 201
 
 
+def validate_post(id: int, post: PostModel | None) -> None:
+    if not post:
+        abort(404, message=f"Post (id={id}) not found")
+
 class Post(Resource):
     @marshal_with(post_fields)
     def get(self, id: int):
-        post = PostsModel.query.filter_by(id=id).first()
-        if not post:
-            abort(404, message=f"Post (id={id}) not found") ##
+        post = PostModel.query.filter_by(id=id).first()
+        validate_post(id, post)
         return post, 200
     
     @marshal_with(post_fields)
     def put(self, id: int):
-        post = PostsModel.query.filter_by(id=id).first()
-        if not post:
-            abort(404, message=f"Post (id={id}) not found")
-
+        post = PostModel.query.filter_by(id=id).first()
+        validate_post(id, post)
         args = post_args.parse_args()
         post.title = args["title"]
         post.content = args["content"]
         post.category = args["category"]
         post.tags = ",".join(args["tags"])
-        post.updatedAt = datetime.now()
         db.session.commit()
         return post, 200
     
     @marshal_with(post_fields)
     def delete(self, id: int):
-        post = PostsModel.query.filter_by(id=id).first()
-        if not post:
-            abort(404, message=f"Post (id={id}) not found")
+        post = PostModel.query.filter_by(id=id).first()
+        validate_post(id, post)
         db.session.delete(post)
         db.session.commit()
         return None, 204
 
 
-api.add_resource(Posts, "/posts")
-api.add_resource(Post, "/posts/<int:id>")
+routes.add_resource(Posts, "/posts")
+routes.add_resource(Post, "/posts/<int:id>")
 
 
 
 if __name__ == "__main__":
-    app.run(debug = True) ## Remove Debug
+    if not isfile(app.config["SQLALCHEMY_DATABASE_URI"].replace("sqlite:///", "./instance/")):
+        with app.app_context():
+            db.create_all()
+    app.run()
